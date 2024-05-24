26ae34389c15939db82e480b53c56b152cfa89ca
SubGameR69e <azjoker333@gmail.com>
2024-05-24 16:28:37 +0100

    Setup Screen

--- 2048 Game/main.py
+++ 2048 Game/main.py
@@ -22,3 +22,20 @@ MOVE_VEL = 20
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048 Game")
+
+def main(WINDOW):
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main(WINDOW)
