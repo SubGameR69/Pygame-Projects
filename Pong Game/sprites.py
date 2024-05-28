556e19062eea187d40a74cb56aaab441fbf71671
SubGameR69e <azjoker333@gmail.com>
2024-05-28 21:09:47 +0100

    wall_collision()

--- Pong Game/sprites.py
+++ Pong Game/sprites.py
@@ -43,5 +43,23 @@ class Ball(pygame.sprite.Sprite):
     def move(self, dt):
         self.rect.center += self.direction * SPEED["ball"] * dt
 
+    def wall_collision(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.direction.y *= -1
+
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.direction.y *= -1
+
+        if self.rect.right >= WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
+            self.direction.x *= -1
+
+        if self.rect.left <= 0:
+            self.rect.left = 0
+            self.direction.x *= -1
+
     def update(self, dt):
         self.move(dt)
+        self.wall_collision()
