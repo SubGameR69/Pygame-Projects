723f8f7fc369e748cc44e285233246713043ed42
SubGameR69e <azjoker333@gmail.com>
2024-05-31 21:59:31 +0100

    Paddle class & Opponent

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -14,6 +14,7 @@ class Game:
         self.paddle_sprites = pygame.sprite.Group()
         self.player = Player((self.all_sprites, self.paddle_sprites))
         self.ball = Ball(self.all_sprites, self.paddle_sprites)
+        self.opponent = Opponent((self.all_sprites, self.paddle_sprites), ball=self.ball)
 
     def run(self):
         while self.running:
--- Pong Game/sprites.py
+++ Pong Game/sprites.py
@@ -1,7 +1,7 @@
 from settings import *
 from random import choice, uniform
 
-class Player(pygame.sprite.Sprite):
+class Paddle(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
 
@@ -13,22 +13,38 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = POS["player"])
         self.old_rect = self.rect.copy()
         self.direction = 0
-        self.speed = SPEED["player"]
 
     def move(self, dt):
         self.rect.centery += self.direction * self.speed * dt
         self.rect.top = 0 if self.rect.top < 0 else self.rect.top
         self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
 
-    def get_direction(self):
-        keys = pygame.key.get_pressed()
-        self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-
     def update(self, dt):
         self.old_rect = self.rect.copy()
         self.get_direction()
         self.move(dt)
 
+
+class Player(Paddle):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.speed = SPEED["player"]
+
+    def get_direction(self):
+        keys = pygame.key.get_pressed()
+        self.direction = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+
+class Opponent(Paddle):
+    def __init__(self, groups, ball):
+        super().__init__(groups)
+        self.speed = SPEED["opponent"]
+        self.rect.center = POS["opponent"]
+        self.ball = ball
+        
+
+    def get_direction(self):
+        self.direction = 1 if self.ball.rect.centery > self.rect.centery else -1
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, groups, paddle_sprites):
