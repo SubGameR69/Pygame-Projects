e4da16f0033b9ab956b65b9e48cade10d849f969
SubGameR69e <azjoker333@gmail.com>
2024-06-10 13:44:17 +0100

    Display Setup

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,1 +1,21 @@
 import pygame
+from config import *
+
+screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+pygame.display.set_caption("Snake Game")
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
+
+    screen.fill(BG_COLOR)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+pygame.quit()
