2e1d5c96457d34256dc06a2aeb65eb5c23f19c60
SubGameR69e <azjoker333@gmail.com>
2024-06-12 03:47:18 +0100

    Snake Move

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,5 +1,6 @@
 import pygame
 from config import *
+from random import randrange
 
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 pygame.display.set_caption("Snake Game")
@@ -8,29 +9,50 @@ running = True
 begin = True
 clock = pygame.time.Clock()
 
+time = None
+
 snake_rect = None
 snake_length = None
 snake_parts = None
 snake_direction = None
+random_pos = randrange(0, SCREEN_SIZE, SNAKE_PART_SIZE)
 
 while running:
     if begin:
         begin = False
-        snake_rect = pygame.rect.Rect(200, 200, SNAKE_PART_SIZE, SNAKE_PART_SIZE)
+        time = 0
+        snake_rect = pygame.rect.Rect(random_pos, random_pos, SNAKE_PART_SIZE, SNAKE_PART_SIZE)
         snake_length = 1
-        snake_parts = [snake_rect]
-        snake_direction = pygame.math.Vector2()
+        snake_parts = []
+        snake_direction = pygame.math.Vector2(0, 0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake_direction = pygame.math.Vector2(0, -SNAKE_MOVE_LENGTH)
+            if event.key == pygame.K_DOWN:
+                snake_direction = pygame.math.Vector2(0, SNAKE_MOVE_LENGTH)
+            if event.key == pygame.K_RIGHT:
+                snake_direction = pygame.math.Vector2(SNAKE_MOVE_LENGTH, 0)
+            if event.key == pygame.K_LEFT:
+                snake_direction = pygame.math.Vector2(-SNAKE_MOVE_LENGTH, 0)
 
     screen.fill(BG_COLOR)
+
+    time_now = pygame.time.get_ticks()
 
     for i in range(0, SCREEN_SIZE, GRID_CELL_SIZE):
         pygame.draw.line(screen, GRID_COLOR, (i, 0), (i, SCREEN_SIZE))
         pygame.draw.line(screen, GRID_COLOR, (0, i), (SCREEN_SIZE, i))
 
-    [pygame.draw.rect(screen, "red", snake_part) for snake_part in snake_parts]
+    if time_now - time > DELAY:
+        time = time_now
+        snake_rect.move_ip(snake_direction)
+        snake_parts.append(snake_rect.copy())
+        snake_parts = snake_parts[-snake_length:]
+
+    [pygame.draw.rect(screen, SNAKE_COLOR, snake_part) for snake_part in snake_parts]
 
     pygame.display.flip()
 
