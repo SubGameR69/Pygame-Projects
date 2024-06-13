e2fd6f8854a50c156eec04fe95c28afdf72e9824
SubGameR69e <azjoker333@gmail.com>
2024-06-13 23:44:13 +0100

    load_assets()

---
+++ Flappy Bird/assets.py
@@ -0,0 +1,14 @@
+import os
+import pygame
+
+sprites = {}
+
+
+def load_sprites():
+    path = os.path.join("assets", "sprites")
+    for file in os.listdir(path):
+        sprites[file.split(".")[0]] = pygame.image.load(os.path.join(path, file))
+
+def get_sprite(name):
+    return sprites[name]
+
