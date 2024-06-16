f86d04fbc097836b29b5ca3982ecd558feb7c61f
SubGameR69e <azjoker333@gmail.com>
2024-06-16 02:38:00 +0100

    Generate Pipes

--- Flappy Bird/main.py
+++ Flappy Bird/main.py
@@ -29,7 +29,7 @@ Column(sprites)
 
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             run = False
 
     WIN.fill("pink")
--- Flappy Bird/objects/column.py
+++ Flappy Bird/objects/column.py
@@ -1,5 +1,7 @@
 import pygame
 import assets
+from settings import *
+import random
 
 class Column(pygame.sprite.Sprite):
 	def __init__(self, *groups):
@@ -8,6 +10,19 @@ class Column(pygame.sprite.Sprite):
 		self.sprite = assets.get_sprite("pipe-green")
 		self.sprite_rect = self.sprite.get_rect()
 
-		self.image = pygame.surface.Surface((self.sprite_rect.width, self.sprite_rect.height))
-		self.image.fill("red")
-		self.rect = self.image.get_rect(topleft=(0, 0))
+		self.pipe_bottom = self.sprite
+		self.pipe_bottom_rect = self.pipe_bottom.get_rect(bottomleft=(0, SCREEN_HEIGHT + self.gap * 2))
+
+		self.pipe_top = pygame.transform.flip(self.sprite, False, True)
+		self.pipe_top_rect = self.pipe_top.get_rect(topleft=(0, 0))
+
+		self.image = pygame.surface.Surface((self.sprite_rect.width, self.sprite_rect.height * 2 + self.gap), pygame.SRCALPHA)
+		self.image.blit(self.pipe_top, self.pipe_top_rect)
+		self.image.blit(self.pipe_bottom, self.pipe_bottom_rect)
+
+		sprite_floor_height = assets.get_sprite("floor").get_rect().height
+
+		min_y = 100
+		max_y = SCREEN_HEIGHT - sprite_floor_height - 100
+
+		self.rect = self.image.get_rect(midleft=(0, random.uniform(min_y, max_y)))
