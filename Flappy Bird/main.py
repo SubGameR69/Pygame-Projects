83535f59920049a1bcd84eecba4141e2db859144
SubGameR69e <azjoker333@gmail.com>
2024-06-17 02:37:15 +0100

    update Pipes

--- Flappy Bird/main.py
+++ Flappy Bird/main.py
@@ -12,6 +12,7 @@ pygame.init()
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
+column_create_event = pygame.USEREVENT
 
 run = True
 
@@ -25,12 +26,16 @@ Background(sprites, index=1)
 Floor(0, sprites)
 Floor(1, sprites)
 
+pygame.time.set_timer(column_create_event, 2400)
+
 Column(sprites)
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             run = False
+        if event.type == column_create_event:
+            Column(sprites)
 
     WIN.fill("pink")
 
