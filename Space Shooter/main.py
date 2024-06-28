3b6e746a0385da8f0d4bb01dfc71ffa3feca87a7
SubGameR69e <azjoker333@gmail.com>
2024-06-28 02:34:47 +0100

    Player & Stars

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -1,11 +1,18 @@
 import pygame
+from random import randint
 
 # General Setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space Shooter")
 
 running = True
+
+player_surf = pygame.image.load("assets/images/player.png").convert_alpha()
+star_surf = pygame.image.load("assets/images/star.png").convert_alpha()
+
+star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
 
 while running:
     # event loop
@@ -14,7 +21,10 @@ while running:
             running = False
 
     # draw the game
-    display_surface.fill("red")
+    display_surface.fill("darkgray")
+    for pos in star_position:
+        display_surface.blit(star_surf, pos)
+    display_surface.blit(player_surf, (200, 200))
     pygame.display.update()
 
 pygame.quit()
