f1873e784741ebb103a13eabe099e32852151a00
SubGameR69e <azjoker333@gmail.com>
2024-07-07 07:21:14 +0100

    Player Class

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -1,6 +1,14 @@
 import pygame
 from random import randint
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/images/player.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+
 # General Setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -9,10 +17,10 @@ pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
 FPS = 60
 
+player = Player()
+
 running = True
 
-player_surf = pygame.image.load("assets/images/player.png").convert_alpha()
-player_rect = player_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 player_direction = pygame.math.Vector2(0, 0)
 player_speed = 300
 
@@ -32,11 +40,16 @@ while running:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("fire laser")
+
     keys = pygame.key.get_pressed()
     player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
     player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
 
-    player_rect.center += player_direction * player_speed * dt
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player.rect.center += player_direction * player_speed * dt
 
     # draw the game
     display_surface.fill("darkgray")
@@ -44,7 +57,7 @@ while running:
         display_surface.blit(star_surf, pos)
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(player_surf, player_rect)
+    display_surface.blit(player.image, player.rect)
 
     # Player Movement
     pygame.display.update()
