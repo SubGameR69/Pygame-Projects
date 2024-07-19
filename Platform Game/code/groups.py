206f2ed0add523bff9d5bb3f0ed5e9b5fac8099e
SubGameR69e <azjoker333@gmail.com>
2024-07-19 21:49:37 +0100

    Camera Offset

---
+++ Platform Game/code/groups.py
@@ -0,0 +1,14 @@
+from settings import *
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.Vector2()
+        
+    def draw(self, target_pos):
+        self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
+        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
+        
+        for sprite in self:
+            self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
--- Platform Game/code/main.py
+++ Platform Game/code/main.py
@@ -1,6 +1,6 @@
 from settings import *
-from sprites import Sprite
-from sprites import Player
+from sprites import *
+from groups import AllSprites
 
 class Game:
     def __init__(self):
@@ -11,7 +11,7 @@ class Game:
         self.running = True
         
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         
         # load game
@@ -28,7 +28,7 @@ class Game:
             
         for obj in tmx_map.get_layer_by_name("Entities"):
             if obj.name == "Player":
-                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
         
     def run(self):
         while self.running:
@@ -43,7 +43,7 @@ class Game:
             
             # draw
             self.display_surface.fill(BG_COLOR)
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
