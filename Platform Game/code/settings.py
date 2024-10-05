535e22c353b15a1b4bc0eb7936f4ed9b6cb4f6d8
SubGameR69e <azjoker333@gmail.com>
2024-10-05 22:17:02 +0100

    project cancelled

--- Platform Game/code/settings.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from os import walk
-from os.path import join
-from pytmx.util_pygame import load_pygame
-
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-TILE_SIZE = 64
-FPS = 60
-BG_COLOR = "#fcdfcd"
