887f4730f9f556bcdf5a9357b4b69459039098e0
SubGameR69e <azjoker333@gmail.com>
2024-10-06 15:00:50 +0100

    create_obstacles()

--- Space Invaders Modern/main.py
+++ Space Invaders Modern/main.py
@@ -1,16 +1,40 @@
 import pygame, sys
 from player import Player
+import obstacle
 
 
 class Game:
     def __init__(self):
+        # player setup
         player_sprite = Player((SCREEN_WIDTH / 2, SCREEN_HEIGHT), SCREEN_WIDTH, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+
+        # obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_offset = [num * (SCREEN_WIDTH / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_offset, x_start=SCREEN_WIDTH / 15, y_start=480)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_idx, row in enumerate(self.shape):
+            for col_idx, col in enumerate(row):
+                if col == "x":
+                    x = x_start + col_idx * self.block_size + offset_x
+                    y = y_start + row_idx * self.block_size
+                    block = obstacle.Block(self.block_size, (241, 79, 80), x, y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
 
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+        self.blocks.draw(screen)
         # update all sprite groups
         # draw all sprite groups
 
