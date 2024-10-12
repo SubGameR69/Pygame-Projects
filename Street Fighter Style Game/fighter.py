0ececbaaa0f7240e0a2a89a81a5bacfb0ccfe90e
SubGameR69e <azjoker333@gmail.com>
2024-10-12 21:42:53 +0100

    load spritesheets

--- Street Fighter Style Game/fighter.py
+++ Street Fighter Style Game/fighter.py
@@ -2,8 +2,15 @@ import pygame
 
 
 class Fighter:
-    def __init__(self, x, y):
-        self.flip = False
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_sprites(sprite_sheet, animation_steps)
+        self.action = 0  # 0: idle 1: run 2: jump 3: attack 4: attack2 5: hit 6: death
+        self.frame_idx = 0
+        self.image = self.animation_list[self.action][self.frame_idx]
         self.rect = pygame.Rect(x, y, 80, 180)
         self.vel_y = 0
         self.jumping = False
@@ -12,6 +19,17 @@ class Fighter:
         self.attack_type = 0
         
         self.health = 100
+
+    def load_sprites(self, sprite_sheet, animation_steps):
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                scaled_img = pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale))
+                temp_img_list.append(scaled_img)
+            animation_list.append(temp_img_list)
+        return animation_list
 
     def move(self, screen_width, screen_height, screen, target):
         speed = 10
@@ -65,4 +83,6 @@ class Fighter:
         pygame.draw.rect(screen, "green", attacking_rect)
 
     def draw(self, screen):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(screen, "red", self.rect)
+        screen.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
--- Street Fighter Style Game/main.py
+++ Street Fighter Style Game/main.py
@@ -13,14 +13,30 @@ clock = pygame.time.Clock()
 
 bg_img = pygame.image.load("./assets/images/background/background.jpg").convert_alpha()
 
-fighter_1 = Fighter(200, 310)
-fighter_2 = Fighter(700, 310)
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
+warrior_sheet = pygame.image.load("./assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("./assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+fighter_1 = Fighter(200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+fighter_2 = Fighter(700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
 
 
 def draw_bg():
     new_bg = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(new_bg, (0, 0))
-    
+
+
 def draw_healthbar(health, x, y):
     ratio = health / 100
     pygame.draw.rect(screen, "white", (x - 2, y - 2, 404, 34))
